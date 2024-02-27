4b98030d33e83bf03c6ef63224c3583ec4befc6c
iblacksassin <156354743+iblacksassin@users.noreply.github.com>
2024-02-27 17:09:18 +0100

    Add files via upload

---
+++ ponzy.js
@@ -0,0 +1,47 @@
+let slides = document.querySelectorAll('.reviews .flex .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+let copy = (textId) => {
+    document.getElementById(textId).select();
+
+    document.execCommand("copy");
+}
+
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+
+var Next1 = document.getElementById("Next1");
+var Back1 = document.getElementById("Back1");
+
+var progress = document.getElementById("progress");
+
+Next1.onclick = function(){
+    Form1.style.left = "-450px";
+    Form2.style.left = "40px";
+    progress.style.width ="240px";
+}
+Back1.onclick = function(){
+    Form1.style.left = "40px";
+    Form2.style.left = "450px";
+    progress.style.width ="120px";
+}
+
+document.getElementById("open-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+});
